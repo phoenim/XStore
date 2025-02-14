cd6416aa90927e076197dce13af35dba03706fb5
bikaar <nima.268183@gmail.com>
2025-02-14 19:31:24 +0330

    .

--- XStore.Application/Services/Products/Queries/GetAllCategories/IGetAllCategoriesService.cs
+++ XStore.Application/Services/Products/Queries/GetAllCategories/IGetAllCategoriesService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using XStore.Application.Services.Products.Queries.GetCategories;
-using XStore.Common.Dto;
-using XStore.Domain.Entities.Products;
+﻿using XStore.Common.Dto;
 
 namespace XStore.Application.Services.Products.Queries.GetAllCategories
 {
