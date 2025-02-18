7ebfff2cd18227094746be1158ad4ff35fc3f088
bikaar <nima.268183@gmail.com>
2025-02-18 18:37:20 +0330

    fix add product view2 (not complete)

--- XStore.WebAdmin/Controllers/ProductController.cs
+++ XStore.WebAdmin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using XStore.Application.Interfaces.FacadPatterns;
+using XStore.Application.Services.Products.Commands.AddNewProduct;
 using XStore.Domain.Entities.Products;
+using XStore.WebAdmin.Views.Product.PageModels;
 
 namespace XStore.WebAdmin.Controllers
 {
@@ -26,9 +28,22 @@ namespace XStore.WebAdmin.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddNewProduct(Product newProduct, int reqFeatures, List<IFormFile> imges)
+        public IActionResult AddNewProduct(AddFeatureToProductDto newProduct, ProductBag pBag)
         {
+            ProductBag product = pBag;
+            AddNewProduct_FeatureDto[] features = newProduct.features;
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddFeatureToProduct(ProductBag newProduct, int reqFeatures)
+        {
+            AddFeatureToProductDto product = new AddFeatureToProductDto();
+            product.Product = newProduct;
+            product.reqFeatures = reqFeatures;
+            product.features = new AddNewProduct_FeatureDto[reqFeatures];
+
+            return View(product);
         }
     }
 }
---
+++ XStore.WebAdmin/Views/Product/PageModels/AddFeatureToProductDto.cs
@@ -0,0 +1,23 @@
+﻿namespace XStore.WebAdmin.Views.Product.PageModels
+{
+    using XStore.Application.Services.Products.Commands.AddNewProduct;
+    using XStore.Domain.Entities.Products;
+    public class AddFeatureToProductDto
+    {
+        public ProductBag Product { get; set; }
+        public AddNewProduct_FeatureDto[] features { get; set; }
+        public int reqFeatures { get; set; }
+
+    }
+
+    public class ProductBag
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public long CategoryId { get; set; }
+        public bool Displayed { get; set; }
+        public double Price { get; set; }
+        public int Inventory { get; set; }
+    }
+}
